e92ba8bb2e7729fe856041c2fc44eeecbed7f908
lxl66566 <18259734087@163.com>
2022-09-11 10:28:40 +0800

    20220911-10:28:40

---
+++ README.md
@@ -0,0 +1,4 @@
+# my-key-data
+very important data of me, to decrease the damage after phisical crash
+
+galgame里面的存档归档记得把`-`和后面的给去了
---
+++ backup.sh
@@ -0,0 +1,14 @@
+rm -r Data-Tai
+rm -r galgame/*
+cp -f D:/Documents/password.kdbx ./
+cp -r -f D:/no_install_software/tai/Data ./Data-Tai
+cp -r -f G:/galgame/苍蓝彼端的四重奏官方中文/SaveData ./galgame/SaveData-AoKana
+cp -r -f G:/galgame/哥哥，早上起床之前都要抱紧我哦！/savedata ./galgame/savedata-onigyu_chs
+cp -r -f G:/galgame/冥契的牧神节/savedata ./galgame/savedata-meikeinoruperukaria
+cp -r -f G:/galgame/魔女的夜宴/savedata ./galgame/savedata-sanobawichi
+cp -r -f "C:/Users/lxl/AppData/Roaming/NEKO WORKs/nekopara3" ./galgame/nekopara3
+cp -r -f "G:/galgame/May-Be SOFT/CHANGE/Save" ./galgame/Save-CHANGE
+git add -A
+git commit -m $(date "+%Y%m%d-%H:%M:%S")
+git push origin main -f
+exec /bin/bash
