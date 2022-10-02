69c9b6325a62ea3c21ab794e5fb5c5c1607ad8de
lxl66566 <18259734087@163.com>
2022-10-02 23:32:56 +0800

    20221002-23:32:56

--- backup.sh
+++ backup.sh
@@ -3,7 +3,6 @@ rm -r galgame/*
 cp -f D:/Documents/password.kdbx ./
 cp -r -f D:/no_install_software/tai1.0.0.5/Data ./Data-Tai
 cp -r -f G:/galgame/哥哥，早上起床之前都要抱紧我哦！/savedata ./galgame/savedata-onigyu_chs
-cp -r -f G:/galgame/冥契的牧神节/savedata ./galgame/savedata-meikeinoruperukaria
 cp -r -f G:/galgame/魔女的夜宴/savedata ./galgame/savedata-sanobawichi
 cp -r -f "C:/Users/lxl/AppData/Roaming/NEKO WORKs/nekopara3" ./galgame/nekopara3
 cp -r -f "G:/galgame/May-Be SOFT/CHANGE/Save" ./galgame/Save-CHANGE
