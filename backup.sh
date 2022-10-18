d28cdfa88d8a2c806de1e30c0bcac8e4b376ce19
lxl66566 <18259734087@163.com>
2022-10-19 00:26:28 +0800

    20221019-00:26:28

--- backup.sh
+++ backup.sh
@@ -7,6 +7,7 @@ cp -rf -u "C:/Users/lxl/AppData/Roaming/NEKO WORKs/nekopara3" ./galgame/nekopara
 cp -rf -u "G:/galgame/May-Be SOFT/CHANGE/Save" ./galgame/CHANGE
 cp -rf -u C:/Users/lxl/AppData/Roaming/RenPy/DDLC-1454445547 ./galgame/DDLC
 cp -rf -u "G:\galgame\灵感满溢的甜蜜创想\savedata_cn" ./galgame/灵感满溢的甜蜜创想
+cp -rf -u "C:\Users\lxl\Saved Games\MoeNovel\A Sky Full of Stars" ./galgame/仰望夜空的星辰
 git add -A
 git commit -m $(date "+%Y%m%d-%H:%M:%S")
 git push origin main
