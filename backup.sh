9c016a23a2a4103f817287c882e66207ca5fd05c
lxl66566 <18259734087@163.com>
2022-11-19 15:25:43 +0800

    20221119-15:25:43

--- backup.sh
+++ backup.sh
@@ -3,11 +3,10 @@ cp -rf -u "D:\no_install_software\Tai1.3.0.0\AppIcons" ./Tai
 cp -f D:/Documents/password.kdbx ./
 cp -rf -u G:/galgame/哥哥，早上起床之前都要抱紧我哦！/savedata ./galgame/哥哥，早上起床之前都要抱紧我哦！
 cp -rf -u G:/galgame/魔女的夜宴/savedata ./galgame/魔女的夜宴
-cp -rf -u "C:/Users/lxl/AppData/Roaming/NEKO WORKs/nekopara3" ./galgame/nekopara3
 cp -rf -u "G:/galgame/May-Be SOFT/CHANGE/Save" ./galgame/CHANGE
 cp -rf -u C:/Users/lxl/AppData/Roaming/RenPy/DDLC-1454445547 ./galgame/DDLC
 cp -rf -u "G:\galgame\灵感满溢的甜蜜创想\savedata_cn" ./galgame/灵感满溢的甜蜜创想
-cp -rf -u "C:\Users\lxl\Saved Games\MoeNovel\A Sky Full of Stars" ./galgame/仰望夜空的星辰
+cp -rf -u "C:\Users\lxl\Saved Games\WillPlus\見上げてごらん、夜空の星を" ./galgame/
 bash compress.sh
 git add -A
 git commit -m $(date "+%Y%m%d-%H:%M:%S")
