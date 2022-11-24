7aa499afdc854b6b62b8b3228db846c5a164725f
lxl66566 <18259734087@163.com>
2022-11-25 00:04:36 +0800

    20221125-00:04:36

--- backup.sh
+++ backup.sh
@@ -5,7 +5,6 @@ cp -rf -u G:/galgame/哥哥，早上起床之前都要抱紧我哦！/savedata .
 cp -rf -u G:/galgame/魔女的夜宴/savedata ./galgame/魔女的夜宴
 cp -rf -u "G:/galgame/May-Be SOFT/CHANGE/Save" ./galgame/CHANGE
 cp -rf -u C:/Users/lxl/AppData/Roaming/RenPy/DDLC-1454445547 ./galgame/DDLC
-cp -rf -u "G:\galgame\灵感满溢的甜蜜创想\savedata_cn" ./galgame/灵感满溢的甜蜜创想
 cp -rf -u "C:\Users\lxl\Saved Games\WillPlus\見上げてごらん、夜空の星を" ./galgame/
 bash compress.sh
 git add -A
