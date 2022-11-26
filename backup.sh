331a5c9886c2a87fa3a4c5d6c5042a3e58e3309d
lxl66566 <18259734087@163.com>
2022-11-27 00:22:26 +0800

    20221127-00:22:26

--- backup.sh
+++ backup.sh
@@ -6,6 +6,7 @@ cp -rf -u G:/galgame/魔女的夜宴/savedata ./galgame/魔女的夜宴
 cp -rf -u "G:/galgame/May-Be SOFT/CHANGE/Save" ./galgame/CHANGE
 cp -rf -u C:/Users/lxl/AppData/Roaming/RenPy/DDLC-1454445547 ./galgame/DDLC
 cp -rf -u "C:\Users\lxl\Saved Games\WillPlus\見上げてごらん、夜空の星を" ./galgame/
+cp -rf -u "G:\galgame\常轨脱离Creative\savedata" ./galgame/常轨脱离Creative
 bash compress.sh
 git add -A
 git commit -m $(date "+%Y%m%d-%H:%M:%S")
