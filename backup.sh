9e8d9b2de8d7e0137225867aef427bfcccd4b82f
Backup <Backup@gmail,com>
2022-12-24 16:12:14 +0800

    20221224-16:12:14

--- backup.sh
+++ backup.sh
@@ -1,12 +1,12 @@
-cp -rfn -u "D:\no_install_software\Tai1.3.0.0\Data" ./Tai
-cp -rfn -u "D:\no_install_software\Tai1.3.0.0\AppIcons" ./Tai
-cp -fn D:/Documents/password.kdbx ./
-cp -rfn -u G:/galgame/哥哥，早上起床之前都要抱紧我哦！/savedata ./galgame/哥哥，早上起床之前都要抱紧我哦！
-cp -rfn -u G:/galgame/魔女的夜宴/savedata ./galgame/魔女的夜宴
-cp -rfn -u "G:/galgame/May-Be SOFT/CHANGE/Save" ./galgame/CHANGE
-cp -rfn -u C:/Users/lxl/AppData/Roaming/RenPy/DDLC-1454445547 ./galgame/DDLC
-cp -rfn -u "C:\Users\lxl\Saved Games\WillPlus\見上げてごらん、夜空の星を" ./galgame/
-cp -rfn -u "G:\galgame\常轨脱离Creative\savedata" ./galgame/常轨脱离Creative
+cp -rfu  "D:\no_install_software\Tai1.3.0.0\Data" ./Tai
+cp -rfu  "D:\no_install_software\Tai1.3.0.0\AppIcons" ./Tai
+cp -fu D:/Documents/password.kdbx ./
+cp -rfu  G:/galgame/哥哥，早上起床之前都要抱紧我哦！/savedata ./galgame/哥哥，早上起床之前都要抱紧我哦！
+cp -rfu  G:/galgame/魔女的夜宴/savedata ./galgame/魔女的夜宴
+cp -rfu  "G:/galgame/May-Be SOFT/CHANGE/Save" ./galgame/CHANGE
+cp -rfu  C:/Users/lxl/AppData/Roaming/RenPy/DDLC-1454445547 ./galgame/DDLC
+cp -rfu  "C:\Users\lxl\Saved Games\WillPlus\見上げてごらん、夜空の星を" ./galgame/
+cp -rfu  "G:\galgame\常轨脱离Creative\savedata" ./galgame/常轨脱离Creative
 bash compress.sh
 git add -A
 git commit -m $(date "+%Y%m%d-%H:%M:%S")
