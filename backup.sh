d84d061cf5bd522515b9610208acf33ff7856b1a
Backup <Backup@gmail,com>
2023-01-06 00:27:26 +0800

    20230106-00:27:26

--- backup.sh
+++ backup.sh
@@ -7,7 +7,7 @@ cp -rfu  "G:/galgame/May-Be SOFT/CHANGE/Save" ./galgame/CHANGE
 cp -rfu  C:/Users/lxl/AppData/Roaming/RenPy/DDLC-1454445547 ./galgame/DDLC
 cp -rfu  "C:\Users\lxl\Saved Games\WillPlus\見上げてごらん、夜空の星を" ./galgame/
 cp -fu  "G:\galgame\真愿朦幻馆～在时间暂停的洋馆里追寻明天的羔羊们～\BGI.gdb" ./galgame/真愿朦幻馆～在时间暂停的洋馆里追寻明天的羔羊们～
-cp -rfu "C:\Users\lxl\AppData\Roaming\ゆずソフトSOUR\PARQUET" ./galgame/PARQUET
+cp -rfu "C:\Users\lxl\AppData\Roaming\ゆずソフトSOUR\PARQUET" ./galgame/
 cp -rfu "C:\Users\lxl\AppData\Local\Microsoft\Edge\User Data\Default\Bookmarks" ./Browser/Bookmarks
 bash compress.sh
 git add -A
