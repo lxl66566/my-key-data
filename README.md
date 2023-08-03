1c0ddc973d7234591fc1409066f6a8a1918442bd
lxl66566 <18259734087@163.com>
2023-08-04 01:25:41 +0800

    clean git

---
+++ README.md
@@ -0,0 +1,23 @@
+# my-key-data
+important data of me, to decrease the damage after phisical crash,
+
+and to memorize my experiences, like galgame times.
+<!-- 
+* Publlic?
+
+To sync my data without github login. Important data has been encrypted. -->
+## 仓库组成
+### browser
+浏览器扩展数据、脚本、收藏夹、history（use *History Trends Unlimited* extension）
+### config
+config files of several windows softwares
+### galgame
+savedata of galgames still playing
+### other_games
+other games' important data
+### Tai
+time record data of my windows computer. needs [Tai](https://github.com/Planshit/Tai) v1.4.0.0, otherwise it maybe not work(like >=1.5.0.0 cannot show website statistics)
+### password.kdbx
+password book, **very important**, 
+### 手心输入法词库
+如题。由于包含敏感信息，故加密。
---
+++ backup.sh
@@ -0,0 +1,35 @@
+# 精简仓库时请不要直接删除！有非 cp 的文件在仓库内！
+# tai
+cp -rfu  "D:\no_install_software\Tai1.3.0.0\Data" ./Tai
+cp -rfu  "D:\no_install_software\Tai1.3.0.0\AppIcons" ./Tai
+
+# single file
+cp -fu D:/Documents/password.kdbx ./
+
+# galgame
+cp -rfu  G:/galgame/哥哥，早上起床之前都要抱紧我哦！/savedata ./galgame/哥哥，早上起床之前都要抱紧我哦！
+cp -rfu  C:/Users/lxl/AppData/Roaming/RenPy/DDLC-1454445547 ./galgame/DDLC
+# 近月少女的礼仪 use savePath so not need to cp it
+# cp -rfu "D:\Documents\近月少女的礼仪" ./galgame
+cp -rfu "G:\galgame\アンレス テルミナリア\save" "./galgame/アンレス テルミナリア"
+
+# config
+    # vscode
+cp -rfu "C:\Users\lxl\AppData\Roaming\Code\User\settings.json" ./config/vscode
+    # v2rayN
+cp -rfu "D:\no_install_software\v2rayN\guiConfigs\config.json" ./config/v2rayN
+cp -rfu "D:\no_install_software\v2rayN\guiConfigs\guiNconfig.json" ./config/v2rayN
+
+# other games
+cp -fu 'D:/software/osu!/collection.db' "./other_games/osu"
+cp -fu 'D:/software/osu!/scores.db' "./other_games/osu"
+
+# browser
+# cp -rfu "C:\Users\lxl\AppData\Local\Microsoft\Edge\User Data\Default\Bookmarks" ./Browser/Bookmarks
+# bash encrypt.sh
+
+git add -A
+git commit -m $(date "+%Y%m%d-%H:%M:%S")
+git push origin main
+git push origin2 main
+exec /bin/bash
