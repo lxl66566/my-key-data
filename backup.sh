daafd3308ad10830468b938bb1507088b07944d5
lxl66566 <18259734087@163.com>
2023-09-17 00:13:38 +0800

    20230917-00:13:37

--- backup.sh
+++ backup.sh
@@ -11,7 +11,6 @@ cp -rfu  C:/Users/lxl/AppData/Roaming/RenPy/DDLC-1454445547 ./galgame/DDLC
 cp -rfu "G:\galgame\アンレス テルミナリア\save" "./galgame/アンレス テルミナリア"
 cp -rfu "C:\Users\lxl\AppData\Roaming\Nitroplus\你与彼女和彼女之恋。" ./galgame
 cp -rfu "C:\Users\lxl\AppData\LocalLow\AnimalHerb\Nekomimi Sweet Housemates\Utage\SaveMyPetProject2" ./galgame/家喵二三事
-cp -rfu "D:\Documents\AliceSoft\兰斯03汉化版V1.00【哈尼喵汉化组】" ./galgame
 
 # config
     # vscode
