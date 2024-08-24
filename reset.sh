89483ab9b551a43d2c8cc8bb5c0727bd40ff0903
lxl66566 <lxl66566@gmail.com>
2024-08-24 20:22:30 +0800

    20240824-20:22:30

--- reset.sh
+++ reset.sh
@@ -1,3 +1,7 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
 read -n 1 -p "Are you sure to clean git and push force? (y/n) " answer
 case ${answer:0:1} in
     y|Y )
@@ -6,10 +10,7 @@ case ${answer:0:1} in
         git remote add origin git@github.com:lxl66566/my-key-data.git
         git add -A
         git commit -m "clean git"
-        git push origin main -f &
-        git push origin2 main -f &
-        wait
-        exec /bin/bash
+        git push origin main -f
     ;;
     * )
         echo "do nothing"
